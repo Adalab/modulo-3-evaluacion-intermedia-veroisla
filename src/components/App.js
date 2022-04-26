1cd265588c356d445275ae98c4a477f3faebcb36
Veronica isla <veroislagleaniz@gmail.com>
2022-04-26 09:28:37 +0200

    function to create new quote

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,27 @@ function App() {
 
   const [data, setData] = useState(friends);
 
+  //PARA CREAR NUEVA TARJETA
+  const [newquote, setNewQuote] = useState({
+    quote: '',
+    character: '',
+  });
+
+  //PREVENIR ENVÍO POR DEFECTO FORM
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
+  //AÑADIR TARJETA
+
+  const handleNewQuote = (ev) => {
+    setNewQuote({
+      ...newquote,
+      [ev.target.name]: ev.target.value,
+    });
+  };
+
+  //RENDERIZAR LISTA
   const htmlData = data.map((phrase, index) => {
     return (
       <li key={index}>
@@ -23,6 +44,26 @@ function App() {
       </header>
       <main>
         <ul>{htmlData}</ul>
+        <form onSubmit={handleSubmit}>
+          <h3>Añadir una nueva frase</h3>
+          <label htmlFor="quote">Frase</label>
+          <input
+            type="text"
+            name="quote"
+            placeholder="Busca tu frase preferida!"
+            onChange={handleNewQuote}
+            value={newquote.quote}
+          ></input>
+          <label htmlFor="character">Personaje</label>
+          <input
+            type="text"
+            name="character"
+            placeholder="Personaje"
+            onChange={handleNewQuote}
+            value={newquote.character}
+          ></input>
+          <button type="button">Añadir</button>
+        </form>
       </main>
     </>
   );
